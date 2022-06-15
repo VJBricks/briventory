7d4be95285cd39aba68b91b86f382f5a80ff626d
Jérôme Varani <2930929+porchy13@users.noreply.github.com>
2022-06-15 15:32:25 +0200

    BrickLinkTokens enhancements

---
+++ app/controllers/accounts/BrickSetTokensForm.java
@@ -0,0 +1,128 @@
+package controllers.accounts;
+
+import models.BrickLinkTokens;
+import play.data.validation.Constraints.MaxLength;
+import play.data.validation.Constraints.Required;
+import play.data.validation.Constraints.Validatable;
+import play.data.validation.ValidationError;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public final class BrickSetTokensForm implements Validatable<List<ValidationError>> {
+
+  // *******************************************************************************************************************
+  // Attributes
+  // *******************************************************************************************************************
+  /** The consumer key. */
+  @Required
+  @MaxLength(database.Constraints.TOKEN_DOMAIN_LENGTH)
+  private String consumerKey;
+
+  /** The consumer secret. */
+  @Required
+  @MaxLength(database.Constraints.TOKEN_DOMAIN_LENGTH)
+  private String consumerSecret;
+
+  /** The access token value. */
+  @Required
+  @MaxLength(database.Constraints.TOKEN_DOMAIN_LENGTH)
+  private String tokenValue;
+
+  /** The access token secret. */
+  @Required
+  @MaxLength(database.Constraints.TOKEN_DOMAIN_LENGTH)
+  private String tokenSecret;
+
+  /** The form contains new entries. */
+  private boolean isNew = true;
+
+  // *******************************************************************************************************************
+  // Construction & Initialization
+  // *******************************************************************************************************************
+
+  /** Creates a new instance of {@link BrickSetTokensForm}. */
+  public BrickSetTokensForm() { /* No-Op */ }
+
+  /**
+   * Creates a new instance of {@link BrickSetTokensForm}.
+   *
+   * @param optionalBricklinkTokens the instance of {@link BrickLinkTokens}, wrapped into an {@link Optional} instance,
+   * to fill this form.
+   */
+  public BrickSetTokensForm(final Optional<BrickLinkTokens> optionalBricklinkTokens) {
+    optionalBricklinkTokens.ifPresent(bricklinkTokens -> {
+      consumerKey = bricklinkTokens.getConsumerKey();
+      consumerSecret = bricklinkTokens.getConsumerSecret();
+      tokenValue = bricklinkTokens.getTokenValue();
+      tokenSecret = bricklinkTokens.getTokenSecret();
+      isNew = false;
+    });
+  }
+
+  // *******************************************************************************************************************
+  // Validatable Overrides
+  // *******************************************************************************************************************
+
+  /**
+   * Validates the various tokens.
+   *
+   * @return the validation errors or an empty list if there is no error.
+   */
+  @Override
+  public List<ValidationError> validate() {
+    return Collections.emptyList();
+  }
+
+  // *******************************************************************************************************************
+  // Getters & Setters
+  // *******************************************************************************************************************
+
+  /** @return {@code true} if it the model does not exist, otherwise {@code false}. */
+  public boolean isNew() { return isNew; }
+
+  /** Sets this form as a filled form. */
+  public void setAsFilled() { isNew = false; }
+
+  /** @return the consumer key. */
+  public String getConsumerKey() { return consumerKey; }
+
+  /**
+   * Sets the consumer key.
+   *
+   * @param consumerKey the consumer key.
+   */
+  public void setConsumerKey(final String consumerKey) { this.consumerKey = consumerKey; }
+
+  /** @return the consumer secret. */
+  public String getConsumerSecret() { return consumerSecret; }
+
+  /**
+   * Sets the consumer secret.
+   *
+   * @param consumerSecret the consumer secret.
+   */
+  public void setConsumerSecret(final String consumerSecret) { this.consumerSecret = consumerSecret; }
+
+  /** @return the token value. */
+  public String getTokenValue() { return tokenValue; }
+
+  /**
+   * Sets the token value.
+   *
+   * @param tokenValue the token value.
+   */
+  public void setTokenValue(final String tokenValue) { this.tokenValue = tokenValue; }
+
+  /** @return the token secret. */
+  public String getTokenSecret() { return tokenSecret; }
+
+  /**
+   * Sets the token secret.
+   *
+   * @param tokenSecret the token secret.
+   */
+  public void setTokenSecret(final String tokenSecret) { this.tokenSecret = tokenSecret; }
+
+}
